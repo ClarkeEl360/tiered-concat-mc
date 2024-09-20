595460d5a734abce244f5556f80f83b290f9dd4a
ClarkeEl360 <ClarkeEl360@northwested.org>
2024-09-20 10:35:10 -0400

    Create concat_mc.py

---
+++ concat_mc.py
@@ -0,0 +1,36 @@
+# Elliot C
+# 9/20/24
+
+from time import sleep
+
+print()
+print()
+print()
+fname = input("What is your first name?")
+fgame = input("Any favorite video game?")
+sentence = "Hello, " + fname + ", player of the hit video game " + fgame + "."
+print(sentence)
+print()
+sleep(1)
+print()
+sleep(1)
+print()
+print("I used concatenation for this.")
+print("Concatenation sucks.")
+sleep(2)
+print("It's anoying, f strings are better.")
+sleep(3)
+print("It's slow, because it takes up to much memory.")
+sleep(3)
+print("It's anoying to code it this way.")
+sleep(3)
+print("It all around is a bad option.")
+print()
+sleep(1)
+print()
+sleep(1)
+print()
+sleep(5)
+bkb = float(75946.32)
+print("Anyways, that will be $" + str(bkb) + " at the first window.")
+print("Thank you for comming to Burger King today.")
